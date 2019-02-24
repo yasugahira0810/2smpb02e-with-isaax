5e9f1c20a6a10f7240a702a4d6c2002b538f8fc7
yasugahira0810 <hourou_freak@yahoo.co.jp>
2019-02-25 00:00:03 +0900

    Update

--- sample2.py
+++ sample2.py
@@ -5,11 +5,22 @@ import time
 import datetime
 import grove_2smpb_02e
 
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+
 AMBIENT_CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
 AMBIENT_WRITE_KEY = os.environ['AMBIENT_WRITE_KEY']
 CHECK_SPAN = int(os.environ.get('CHECK_SPAN', '30'))
 
 sensor = grove_2smpb_02e.Grove2smpd02e()
+app = Flask(__name__)
+
+@app.route('/sensor')
+def cpu():
+    d = am.read
+   #return jsonify(temperature=1, pressure=1)
+    return jsonify(temperature=round(d,2)
 
 am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
 
